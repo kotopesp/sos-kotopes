079b23d2415286268bca594ca242c751f63888e1
yaromochka <yaromochka@yandex.ru>
2024-07-17 03:13:37 +0300

    ready to make ad pets

---
+++ frontend/src/app/common-ui/ad-pet/ad-pet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-pet',
+  standalone: true,
+  imports: [],
+  templateUrl: './ad-pet.component.html',
+  styleUrl: './ad-pet.component.scss'
+})
+export class AdPetComponent {
+
+}
