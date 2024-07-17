5022b8dbad05344bce0fc778b070f24f0d392a6a
yaromochka <yaromochka@yandex.ru>
2024-07-17 03:12:52 +0300

    make questions

---
+++ frontend/src/app/common-ui/question/question.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-question',
+  standalone: true,
+  imports: [
+    NgIf,
+    NgForOf
+  ],
+  templateUrl: './question.component.html',
+  styleUrl: './question.component.scss'
+})
+export class QuestionComponent {
+  @Input() questionItem: any
+  active: string = "active";
+  notActive: string = "";
+  hideAnswer: boolean = false;
+}
