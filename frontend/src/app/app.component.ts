edc6aa26eb24720672312658e10477e7ac6950e5
yaromochka <yaromochka@yandex.ru>
2024-07-17 03:12:26 +0300

    update routes and app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgForOf, NgIf} from "@angular/common";
+import {StickerComponent} from "./common-ui/sticker/sticker.component";
+import {QuestionComponent} from "./common-ui/question/question.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AdsComponent} from "./ads/ads.component";
+import {StartPageComponent} from "./start-page/start-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: StartPageComponent
+  },
+  {
+    path: 'ads', component: AdsComponent
+  }
+];
