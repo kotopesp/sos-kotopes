6c238ffe86a95652ec85115b1002ccfb8ae9dfcc
Lkk9 <l.t.446kk@gmail.com>
2024-07-19 16:05:33 +0300

    add: basic structure for keepers

---
+++ backend/internal/service/keeper/keeper.go
@@ -0,0 +1,35 @@
+package keeper
+
+import (
+	"context"
+
+	"gitflic.ru/spbu-se/sos-kotopes/internal/core"
+)
+
+type service struct {
+	keeperStore core.KeeperStore
+}
+
+func New(keeperStore core.KeeperStore) core.KeeperService {
+	return &service{keeperStore: keeperStore}
+}
+
+func (s *service) GetAll(ctx context.Context, params core.GetAllKeepersParams) ([]core.Keeper, int, error) {
+	return s.keeperStore.GetAll(ctx, params)
+}
+
+func (s *service) GetByID(ctx context.Context, id int) (core.Keeper, error) {
+	return s.keeperStore.GetByID(ctx, id)
+}
+
+func (s *service) Create(ctx context.Context, keeper core.Keeper) error {
+	return s.keeperStore.Create(ctx, keeper)
+}
+
+func (s *service) DeleteById(ctx context.Context, id int) error {
+	return s.keeperStore.DeleteById(ctx, id)
+}
+
+func (s *service) UpdateById(ctx context.Context, id int) error {
+	return s.keeperStore.UpdateById(ctx, id)
+}
