10d44706fcfb3950dcc42c925bcb6544075f5b45
suvorovrain <rodion.suvorov.94@mail.ru>
2024-07-24 15:09:15 +0000

    feat: add intrefaces for favourite users

--- backend/internal/core/user.go
+++ backend/internal/core/user.go
@@ -22,4 +22,20 @@ type (
 		GetUser(ctx context.Context, id int) (user User, err error)
 		GetUserPosts(ctx context.Context, id int) (posts []Post, err error)
 	}
+	FavouriteUser struct {
+		Id        int    `gorm:"primary key;autoIncrement" db:"id"`
+		personId  int    `db:"person_id"`
+		userId    int    `db:"user_id"`
+		createdAt string `db:"created_at"`
+	}
+	FavouriteUserStore interface {
+		AddPersonToFavourite(ctx context.Context, personId int, userId int) (err error)
+		GetFavouritePersons(ctx context.Context, userId int) (persons []FavouriteUser, err error)
+		DeletePersonFromFavourite(ctx context.Context, personId int, userId int) (err error)
+	}
+	FavouriteUserService interface {
+		AddPersonToFavourite(ctx context.Context, personId int, userId int) (err error)
+		GetFavouritePersons(ctx context.Context, userId int) (persons []FavouriteUser, err error)
+		DeletePersonFromFavourite(ctx context.Context, personId int, userId int) (err error)
+	}
 )
