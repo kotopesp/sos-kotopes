8af7119938c57b7c7c25902a52561da37eca8f3a
dmitriich <chdmitry2@gmail.com>
2024-07-25 22:40:17 +0300

    add: keeper methods

--- backend/internal/controller/http/keeper.go
+++ backend/internal/controller/http/keeper.go
@@ -2,11 +2,13 @@ package http
 
 import (
 	"strconv"
+	"time"
 
 	"gitflic.ru/spbu-se/sos-kotopes/internal/controller/http/model"
 	"gitflic.ru/spbu-se/sos-kotopes/internal/core"
 	"gitflic.ru/spbu-se/sos-kotopes/pkg/logger"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func (r *Router) getKeepers(ctx *fiber.Ctx) error {
@@ -21,18 +23,18 @@ func (r *Router) getKeepers(ctx *fiber.Ctx) error {
 	if location := ctx.Query("location"); location != "" {
 		params.Location = &location
 	}
-	if minRating := ctx.Query("minRating"); minRating != "" {
-		rating, err := strconv.ParseFloat(minRating, 64)
-		if err == nil {
-			params.MinRating = &rating
-		}
-	}
-	if maxRating := ctx.Query("maxRating"); maxRating != "" {
-		rating, err := strconv.ParseFloat(maxRating, 64)
-		if err == nil {
-			params.MaxRating = &rating
-		}
-	}
+	// if minRating := ctx.Query("minRating"); minRating != "" {
+	// 	rating, err := strconv.ParseFloat(minRating, 64)
+	// 	if err == nil {
+	// 		params.MinRating = &rating
+	// 	}
+	// }
+	// if maxRating := ctx.Query("maxRating"); maxRating != "" {
+	// 	rating, err := strconv.ParseFloat(maxRating, 64)
+	// 	if err == nil {
+	// 		params.MaxRating = &rating
+	// 	}
+	// }
 	if limit := ctx.Query("limit"); limit != "" {
 		l, err := strconv.Atoi(limit)
 		if err == nil {
@@ -76,23 +78,81 @@ func (r *Router) getKeeperByID(ctx *fiber.Ctx) error {
 }
 
 func (r *Router) createKeeper(ctx *fiber.Ctx) error {
-	usrCtx := ctx.UserContext()
-	err := r.keeperService.Create(&usrCtx, core.Keepers{
-		ID:          0,
-		UserID:      0,
-		Description: "hello keeper",
-		Location:    "asdasd",
-	})
-	if err != nil {
-		return err
+	var keeper core.Keepers
+
+	// parse keeper
+	if err := ctx.BodyParser(&keeper); err != nil {
+		logger.Log().Debug(ctx.UserContext(), err.Error())
+		return ctx.Status(fiber.StatusBadRequest).JSON(model.ErrorResponse(err.Error()))
 	}
-	return nil
+
+	// make sure the create time is set
+	if keeper.CreatedAt.IsZero() {
+		keeper.CreatedAt = time.Now()
+	}
+
+	// create keeper
+	usrCtx := ctx.UserContext()
+	if err := r.keeperService.Create(&usrCtx, keeper); err != nil {
+		logger.Log().Debug(ctx.UserContext(), err.Error())
+		return ctx.Status(fiber.StatusInternalServerError).JSON(model.ErrorResponse(err.Error()))
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(model.OKResponse(keeper))
 }
 
 func (r *Router) updateKeeperById(ctx *fiber.Ctx) error {
-	panic("impl")
+	// get id
+	idStr := ctx.Params("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		logger.Log().Debug(ctx.UserContext(), err.Error())
+		return ctx.Status(fiber.StatusBadRequest).JSON(model.ErrorResponse(err.Error()))
+	}
+
+	var keeper core.Keepers
+
+	// parse keeper
+	if err := ctx.BodyParser(&keeper); err != nil {
+		logger.Log().Debug(ctx.UserContext(), err.Error())
+		return ctx.Status(fiber.StatusBadRequest).JSON(model.ErrorResponse(err.Error()))
+	}
+
+	keeper.ID = id
+
+	// update
+	var usrCtx = ctx.UserContext()
+	if err := r.keeperService.UpdateById(&usrCtx, keeper); err != nil {
+		logger.Log().Debug(ctx.UserContext(), err.Error())
+		if err == gorm.ErrRecordNotFound {
+			return ctx.Status(fiber.StatusNotFound).JSON(model.ErrorResponse(err.Error()))
+		}
+
+		return ctx.Status(fiber.StatusInternalServerError).JSON(model.ErrorResponse(err.Error()))
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(model.OKResponse(keeper))
 }
 
 func (r *Router) deleteKeeperById(ctx *fiber.Ctx) error {
-	panic("impl")
+	// get id
+	idStr := ctx.Params("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		logger.Log().Debug(ctx.UserContext(), err.Error())
+		return ctx.Status(fiber.StatusBadRequest).JSON(model.ErrorResponse(err.Error()))
+	}
+
+	// delete
+	var usrCtx = ctx.UserContext()
+	if err := r.keeperService.DeleteById(&usrCtx, id); err != nil {
+		logger.Log().Debug(ctx.UserContext(), err.Error())
+		if err == gorm.ErrRecordNotFound {
+			return ctx.Status(fiber.StatusNotFound).JSON(model.ErrorResponse(err.Error()))
+		}
+
+		return ctx.Status(fiber.StatusInternalServerError).JSON(model.ErrorResponse(err.Error()))
+	}
+
+	return ctx.SendStatus(fiber.StatusNoContent)
 }
