2d8d9458ad0b30a4d681c8d4c8c9e2d6b09e2cac
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-07-25 19:56:46 +0300

    fix: replace sidebar to widgets folder and add SidebarComponent to posts.component.ts

--- frontend/src/pages/posts/ui/post/post.component.ts
+++ frontend/src/pages/posts/ui/post/post.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { SidebarComponent } from "../../../../widgets/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-post',
--- frontend/src/pages/posts/ui/posts.component.ts
+++ frontend/src/pages/posts/ui/posts.component.ts
@@ -1,14 +1,16 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from "../../../widgets/header/header.component";
 import {PostComponent} from "./post/post.component";
+import { SidebarComponent } from "../../../widgets/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-posts',
   standalone: true,
   imports: [
     HeaderComponent,
-    PostComponent
-  ],
+    PostComponent,
+    SidebarComponent
+],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
