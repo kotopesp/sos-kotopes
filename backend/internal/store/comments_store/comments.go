b04f2b89116d8b0de019a1ef533360b9c0f7ae39
kudrin-chris <c-cud@mail.ru>
2024-07-26 18:14:06 +0300

    feat(comments): methods get, create, update, delete comments implemented in comments store

---
+++ backend/internal/store/comments_store/comments.go
@@ -0,0 +1,69 @@
+package comments
+
+import (
+	"context"
+	"time"
+
+	"gitflic.ru/spbu-se/sos-kotopes/internal/core"
+	"gitflic.ru/spbu-se/sos-kotopes/pkg/postgres"
+	"github.com/gofiber/fiber/v2"
+)
+
+type store struct {
+	*postgres.Postgres
+}
+
+func NewCommentsStore(pg *postgres.Postgres) core.CommentsStore {
+	return &store{
+		pg,
+	}
+}
+
+func (s *store) GetCommentsByPostID(ctx context.Context, params core.GetAllParamsComments, post_id int) ([]core.Comments, error) {
+
+	var comments []core.Comments
+
+	query := s.DB.WithContext(ctx).Model(&core.Comments{})
+
+	if params.Limit != nil {
+		query = query.Limit(*params.Limit)
+	}
+
+	if params.Offset != nil {
+		query = query.Offset(*params.Offset)
+	}
+
+	if err := query.Order("replay_to_comment, reply_to_reply").Find(&comments).Error; err != nil {
+		return nil, fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+	return comments, nil
+
+}
+
+func (s *store) CreateComment(ctx context.Context, comment core.Comments, post_id int) (core.Comments, error) {
+	/*if comment.Is_deleted == true {
+		return comment, fiber.NewError(fiber.StatusBadRequest, "invalid is_deleted comment")
+	}*/
+	if err := s.DB.WithContext(ctx).Create(&comment).Error; err != nil {
+		return comment, fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+	return comment, nil
+}
+
+func (s *store) UpdateComments(ctx context.Context, comment core.Comments) (core.Comments, error) {
+	if err := s.DB.WithContext(ctx).Save(&comment).Error; err != nil {
+		return comment, fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+	return comment, nil
+}
+
+func (s *store) DeleteComments(ctx context.Context, id int) error {
+	deletedComment := core.Comments{
+		Is_deleted: true,
+		Deleted_at: time.Now(),
+	}
+	if err := s.DB.WithContext(ctx).Save(&deletedComment).Error; err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+	return nil
+}
