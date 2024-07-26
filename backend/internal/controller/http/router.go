0516587d4cf35be087cdb25f3e8be1c85b34d089
kudrin-chris <c-cud@mail.ru>
2024-07-26 17:55:23 +0300

    feat(comments): added endpoint for comments

--- backend/internal/controller/http/router.go
+++ backend/internal/controller/http/router.go
@@ -7,20 +7,23 @@ import (
 )
 
 type Router struct {
-	app           *fiber.App
-	entityService core.EntityService
-	authService   interface{}
+	app             *fiber.App
+	entityService   core.EntityService
+	authService     interface{}
+	commentsService core.CommentsService
 }
 
 func NewRouter(
 	app *fiber.App,
 	entityService core.EntityService,
 	authService interface{},
+	commentsService core.CommentsService,
 ) {
 	router := &Router{
-		app:           app,
-		entityService: entityService,
-		authService:   authService,
+		app:             app,
+		entityService:   entityService,
+		authService:     authService,
+		commentsService: commentsService,
 	}
 
 	router.initRequestMiddlewares()
@@ -38,6 +41,12 @@ func (r *Router) initRoutes() {
 	// entities
 	v1.Get("/entities", r.getEntities)
 	v1.Get("/entities/:id", r.getEntityByID)
+
+	//comments
+	v1.Get("/posts/:post_id/comments", r.getCommentsByPostID)
+	v1.Post("/comments", r.createComment)
+	v1.Put("/posts/:post_id/comments/:comment_id", r.updateComment)
+	v1.Delete("/comments/:comment_id", r.deleteComment)
 }
 
 // initRequestMiddlewares initializes all middlewares for http requests
