99e8b5c6cae48b16536ea310cf87f5f819b684c7
kudrin-chris <c-cud@mail.ru>
2024-07-26 18:10:06 +0300

    feat(comments): methods get, create, update, delete comments implemented in comments service

--- backend/internal/service/comments_service/comments.go
+++ backend/internal/service/comments_service/comments.go
@@ -17,15 +17,19 @@ func NewCommentsService(store core.CommentsStore) core.CommentsService {
 }
 
 func (s *commentsService) GetCommentsByPostID(ctx context.Context, params core.GetAllParamsComments, post_id int) ([]core.Comments, error) {
-
+	comments, err := s.CommentsStore.GetCommentsByPostID(ctx, params, post_id)
+	if err != nil {
+		//TODO обработать
+	}
+	return comments, nil
 }
 
 func (s *commentsService) CreateComment(ctx context.Context, comment core.Comments, post_id int) (core.Comments, error) {
-
+	return s.CommentsStore.CreateComment(ctx, comment, post_id)
 }
 func (s *commentsService) UpdateComments(ctx context.Context, comments core.Comments) (core.Comments, error) {
-
+	return s.CommentsStore.UpdateComments(ctx, comments)
 }
 func (s *commentsService) DeleteComments(ctx context.Context, id int) error {
-
+	return s.CommentsStore.DeleteComments(ctx, id)
 }
