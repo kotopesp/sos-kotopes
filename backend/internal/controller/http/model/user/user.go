a1997c9c290af5da9332fedc1400dff5681ae8f6
daniel-zolin <daniel.zolin@yandex.ru>
2024-07-26 22:42:36 +0300

    fix(auth): added some more validation to user struct

--- backend/internal/controller/http/model/user/user.go
+++ backend/internal/controller/http/model/user/user.go
@@ -2,11 +2,11 @@ package user
 
 type (
 	User struct {
-		Username    string  `form:"username" validate:"required,max=25,no_specials"`
+		Username    string  `form:"username" validate:"required,max=50,no_specials"`
 		Password    string  `form:"password" validate:"required,min=8,max=72,contains_digit,contains_uppercase"`
 		Description *string `form:"description"`
-		Firstname   *string `form:"firstname"`
-		Lastname    *string `form:"lastname"`
+		Firstname   *string `form:"firstname" validate:"omitempty,max=25"`
+		Lastname    *string `form:"lastname" validate:"omitempty,max=25"`
 		Photo       *[]byte
 	}
 )
