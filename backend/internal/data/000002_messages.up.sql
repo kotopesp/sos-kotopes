59b195c4e135e74d168e9d38d43bab4ade75c5f0
Homka122 <kombaev02@gmail.com>
2024-07-27 16:08:34 +0300

    feat(migrations): add roles for 'chat_members' and update timestamp

--- backend/internal/data/000002_messages.up.sql
+++ backend/internal/data/000002_messages.up.sql
@@ -1,5 +1,11 @@
+CREATE TYPE chat_member_roles AS ENUM ('admin', 'user');
+
 ALTER TABLE IF EXISTS messages
     ADD content VARCHAR NOT NULL DEFAULT '';
 
 ALTER TABLE IF EXISTS messages
     ALTER COLUMN content DROP DEFAULT;
+
+ALTER TABLE IF EXISTS chat_members
+    ADD role chat_member_roles NOT NULL DEFAULT 'user',
+    ADD updated_at TIMESTAMP NOT NULL DEFAULT now();
