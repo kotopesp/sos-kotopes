69042dd488db82097c5de6fe41e3862fa7cc5af0
yaromochka <yaromochka@yandex.ru>
2024-07-27 12:20:56 +0300

    component for message overlay

---
+++ frontend/src/widgets/header/ui/message-popup/ui/list-role/list-role.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-role',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-role.component.html',
+  styleUrl: './list-role.component.scss'
+})
+export class ListRoleComponent {
+
+}
