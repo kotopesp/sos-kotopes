34bc5585f0766eee086100d8e9c41342d6440a63
yaromochka <yaromochka@yandex.ru>
2024-07-27 12:20:40 +0300

    background to message overlay

---
+++ frontend/src/widgets/header/ui/message-popup/message-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-popup.component.html',
+  styleUrl: './message-popup.component.scss'
+})
+export class MessagePopupComponent {
+
+}
