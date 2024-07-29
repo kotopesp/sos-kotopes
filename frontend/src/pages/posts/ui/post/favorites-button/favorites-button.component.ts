61e16e0c320b110e6cd34d8c0f3fbb35dec75fdd
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-07-29 12:47:22 +0300

    feat(favorites-button): add button view toggling

--- frontend/src/pages/posts/ui/post/favorites-button/favorites-button.component.ts
+++ frontend/src/pages/posts/ui/post/favorites-button/favorites-button.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-favorites-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './favorites-button.component.html',
   styleUrl: './favorites-button.component.scss'
 })
 export class FavoritesButtonComponent {
+  isToggled: boolean = false;
 
+  toggleButton() {
+    this.isToggled = !this.isToggled;
+  }
 }
