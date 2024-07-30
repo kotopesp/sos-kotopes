5092818b3535e279fc1313dccfb878c66bcb3cb2
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-07-30 17:52:07 +0300

    feat: add find status filters in filter bar

--- frontend/src/widgets/filters-bar/filters-bar.component.ts
+++ frontend/src/widgets/filters-bar/filters-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FindStatusComponent } from "./find-status/find-status.component";
+import { FindStatusBottonComponent } from "./find-status/find-status-botton/find-status-botton.component";
 
 @Component({
   selector: 'app-filters-bar',
   standalone: true,
-  imports: [FindStatusComponent],
+  imports: [FindStatusComponent, FindStatusBottonComponent],
   templateUrl: './filters-bar.component.html',
   styleUrl: './filters-bar.component.scss'
 })
