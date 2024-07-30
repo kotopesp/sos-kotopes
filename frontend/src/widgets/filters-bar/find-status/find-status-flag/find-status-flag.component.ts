58579d583db4d60d9f7df6e30809e7e4da729002
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-07-30 18:09:43 +0300

    feat: add find-status-flag.component for post

---
+++ frontend/src/widgets/filters-bar/find-status/find-status-flag/find-status-flag.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NgClass } from "@angular/common";
+
+@Component({
+  selector: 'app-find-status-flag',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './find-status-flag.component.html',
+  styleUrl: './find-status-flag.component.scss'
+})
+export class FindStatusFlagComponent {
+  // here we get data from the backend, ex. we get initialState = "Найден"
+  initialStateArr = ["Пропал", "Найден", "Ищет дом"]
+  initialState: string = this.initialStateArr[Math.floor(Math.random() * 3)]
+
+  ngOnInit() {
+    this.setFlagClass();
+  }
+
+  petStatusClass: string = ""
+  buttonLabel: string = ""
+  setFlagClass() {
+    switch(this.initialState) {
+      case "Пропал": {
+        this.buttonLabel = "Пропал"
+        this.petStatusClass = "lost_status"
+        break
+      }
+
+      case "Найден": {
+        this.buttonLabel = "Найден"
+        this.petStatusClass = "found-home_status"
+        break
+      }
+
+      case "Ищет дом": {
+        this.buttonLabel = "Ищет дом"
+        this.petStatusClass = "looking-for-home_status"
+        break
+      }
+    }
+  }
+
+}
