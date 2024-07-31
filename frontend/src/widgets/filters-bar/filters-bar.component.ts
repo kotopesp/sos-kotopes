59754f3687301f117fbec1df752e956e77b63852
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-07-31 23:50:23 +0300

    feat: add filters-bar's layout

--- frontend/src/widgets/filters-bar/filters-bar.component.ts
+++ frontend/src/widgets/filters-bar/filters-bar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { FindStatusComponent } from "./find-status/find-status.component";
 import { FindStatusBottonComponent } from "./find-status/find-status-botton/find-status-botton.component";
 
 @Component({
   selector: 'app-filters-bar',
   standalone: true,
-  imports: [FindStatusComponent, FindStatusBottonComponent],
+  imports: [FindStatusBottonComponent],
   templateUrl: './filters-bar.component.html',
   styleUrl: './filters-bar.component.scss'
 })
