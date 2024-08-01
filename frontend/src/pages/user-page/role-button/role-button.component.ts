a0f7f905b2fc19f7add4149f8ece9c5b1b0c660a
kinokotakenoko9 <chdmitry2@gmail.com>
2024-08-01 16:21:11 +0300

    add: structure

---
+++ frontend/src/pages/user-page/role-button/role-button.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-role-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './role-button.component.html',
+  styleUrl: './role-button.component.scss'
+})
+export class RoleButtonComponent {
+  @Input() label = ''
+  @Input() buttonColor = ''
+  @Input() icon = ''
+  @Output() onClick = new EventEmitter<any>()
+
+  iconUrl = ''
+  ngOnInit() {
+    this.iconUrl = `url("${this.icon}")`
+  }
+  onClickButton(event: any) {
+    this.onClick.emit(event)
+  }
+}
--- frontend/src/pages/user-page/user-page.component.ts
+++ frontend/src/pages/user-page/user-page.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 
 import {HeaderComponent} from "../../widgets/header/header.component";
 import { UserService } from '../../services/user-service/user.service';
+import { RoleButtonComponent } from './role-button/role-button.component';
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RoleButtonComponent],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.scss'
 })
 export class UserPageComponent {
-  
+  firstName = 'John'
+  secondName = 'Johnson'
+  onlineStatus = 'online'
+  username = 'tim.violine'
+  totalPosts = '22'
+  profilePhoto = '../../assets/images/test-cat.png'
 }
