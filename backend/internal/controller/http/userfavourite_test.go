01ec7ed89e662801ba673eefdcd655308ad47dbc
suvorovrain <rodion.suvorov.94@mail.ru>
2024-08-13 13:43:28 +0000

    test: create signatures for tests

---
+++ backend/internal/controller/http/userfavourite_test.go
@@ -0,0 +1,21 @@
+package http
+
+import "testing"
+
+func TestAddUserToFavourites(T *testing.T) {
+	app, dependencies := newTestApp(t)
+
+	const route = "/api/v1/auth/login"
+}
+
+func TestGetFavouriteUsers(T *testing.T) {
+	app, dependencies := newTestApp(t)
+
+	const route = "/api/v1/auth/login"
+}
+
+func TestDeleteUserFromFavourites(T *testing.T) {
+	app, dependencies := newTestApp(t)
+
+	const route = "/api/v1/auth/login"
+}
