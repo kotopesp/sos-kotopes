cba01c6ef6e4efdab2cbf8abb46a788480056d16
yaromochka <yaromochka@yandex.ru>
2024-08-16 09:55:18 +0300

    little bug fix with header position

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import {Routes} from '@angular/router';
-import {PostsComponent} from "../pages/posts/ui/posts.component";
-import {StartPageComponent} from "../pages/start-page/ui/start-page.component";
+import { PostsComponent } from "../pages/posts/ui/posts.component";
+import { StartPageComponent } from "../pages/start-page/ui/start-page.component";
 import { UserPageComponent } from '../pages/user-page/user-page.component';
 
 export const routes: Routes = [
