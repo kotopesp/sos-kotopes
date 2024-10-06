924648cd189e1d75f4eebd7c2fd02ba5190e866a
Roman <137483082+yaromochka@users.noreply.github.com>
2024-10-06 23:26:01 +0300

    feat: create directive for choosing one element and change his CSS styles

---
+++ frontend/src/directives/choose-one/choose-one.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ChooseOneDirective } from './choose-one.directive';
+
+describe('ChooseOneDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ChooseOneDirective();
+    expect(directive).toBeTruthy();
+  });
+});
