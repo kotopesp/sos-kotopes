eceda385e88acf3c164db3035e04ea08e2885d0e
Roman <137483082+yaromochka@users.noreply.github.com>
2024-10-08 22:19:22 +0300

    temp: make router to create post on posts-page and from profile

--- frontend/src/pages/posts/ui/posts.component.ts
+++ frontend/src/pages/posts/ui/posts.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {HeaderComponent} from "../../../widgets/header/header.component";
 import { PostComponent } from "../../../shared/post/post.component";
 import {FiltersBarComponent} from "../../../widgets/filters-bar/filters-bar.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-posts-services',
@@ -9,8 +10,9 @@ import {FiltersBarComponent} from "../../../widgets/filters-bar/filters-bar.comp
   imports: [
     HeaderComponent,
     PostComponent,
-    FiltersBarComponent
-],
+    FiltersBarComponent,
+    RouterLink
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
