e49fe175fd76d975e37e2280d192901a97c71d06
Urtix <es.perevalov@mail.ru>
2024-11-12 20:22:29 +0300

    feat(service, store): Add an implementation for seeker
    Signed-off-by: Urtix <es.perevalov@mail.ru>

---
+++ backend/internal/service/seeker/seeker.go
@@ -0,0 +1,26 @@
+package seeker
+
+import (
+	"context"
+	"github.com/kotopesp/sos-kotopes/internal/core"
+)
+
+type service struct {
+	seekersStore core.SeekersStore
+}
+
+func New(seekersStore core.SeekersStore) core.SeekersService {
+	return &service{seekersStore: seekersStore}
+}
+
+func (s *service) CreateSeeker(ctx context.Context, seeker core.Seekers) (core.Seekers, error) {
+	return s.seekersStore.CreateSeeker(ctx, seeker)
+}
+
+func (s *service) GetSeeker(ctx context.Context, id int) (core.Seekers, error) {
+	return s.seekersStore.GetSeeker(ctx, id)
+}
+
+func (s *service) UpdateSeeker(ctx context.Context, seeker core.UpdateSeekers) (core.Seekers, error) {
+	return s.seekersStore.UpdateSeeker(ctx, seeker)
+}
