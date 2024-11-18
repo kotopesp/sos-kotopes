d5d47b4bd1ae14e1c5b767b94e977df118eff756
yaromochka <137483082+yaromochka@users.noreply.github.com>
2024-11-19 00:26:46 +0300

    feat: update click-to-select directive and write button state in create post page

--- frontend/src/directives/click-to-select/click-to-select.directive.ts
+++ frontend/src/directives/click-to-select/click-to-select.directive.ts
@@ -1,24 +1,36 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2, OnInit } from '@angular/core';
+import { ButtonStateService } from "../../services/button-state/button-state.service";
 
 @Directive({
   standalone: true,
   selector: '[appClickToSelect]'
 })
-export class ClickToSelectDirective {
+export class ClickToSelectDirective implements OnInit {
   @Input('appClickToSelect') classNameSelect!: string;
+  @Input() buttonIndex!: number;
+  private className!: string;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private buttonState: ButtonStateService
+  ) {}
+
+  ngOnInit() {
+    this.className = this.el.nativeElement.className;
+    const savedButtonIndex = this.buttonState.getState(this.className);
+    if (savedButtonIndex !== null && savedButtonIndex === this.buttonIndex) {
+      this.renderer.addClass(this.el.nativeElement, this.classNameSelect);
+    }
+  }
 
   @HostListener('click') onClick() {
     const element = this.el.nativeElement;
-
-    // Удаляем класс со всех других элементов, у которых он есть
+    this.buttonState.setState(this.className, this.buttonIndex);
     const elementsWithClass = document.querySelectorAll(`.${this.classNameSelect}`);
-    elementsWithClass.forEach(el => {
+    elementsWithClass.forEach((el) => {
       this.renderer.removeClass(el, this.classNameSelect);
     });
-
-    // Переключаем класс на текущем элементе
     if (!element.classList.contains(this.classNameSelect)) {
       this.renderer.addClass(element, this.classNameSelect);
     } else {
--- frontend/src/services/button-state/button-state.service.ts
+++ frontend/src/services/button-state/button-state.service.ts
@@ -5,15 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class ButtonStateService {
   private states: { [groupKey: string]: number | null } = {};
-
-  // Получить состояние кнопки по группе
   getState(groupKey: string): number | null {
-    // Преобразуем строку в число, если она есть, иначе возвращаем null
     const value = this.states[groupKey];
     return value !== undefined ? Number(value) : null;
   }
 
-  setState(groupKey: string, buttonIndex: number): void {
-    this.states[groupKey] = buttonIndex;  // Сохраняем как число
+  setState(groupKey: string, buttonIndex: number | null): void {
+    this.states[groupKey] = buttonIndex;
   }
 }
