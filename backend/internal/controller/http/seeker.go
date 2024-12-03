f7e363de2721ee32f045a398e93c1baa5e2a534d
Urtix <es.perevalov@mail.ru>
2024-12-03 17:17:45 +0300

    feat(http): Add unimplemented functions to process the response. Add these functions to router
    Signed-off-by: Urtix <es.perevalov@mail.ru>

---
+++ backend/internal/controller/http/seeker.go
@@ -0,0 +1,11 @@
+package http
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func (r *Router) getSeeker(ctx *fiber.Ctx) error { return nil }
+
+func (r *Router) createSeeker(ctx *fiber.Ctx) error { return nil }
+
+func (r *Router) updateSeeker(ctx *fiber.Ctx) error { return nil }
