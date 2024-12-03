67317221640986a6ab6da569b4bf9fb8903c4640
Urtix <es.perevalov@mail.ru>
2024-12-03 17:00:26 +0300

    test(testutils_test): Add mockSeekerService
    Signed-off-by: Urtix <es.perevalov@mail.ru>

--- backend/internal/controller/http/testutils_test.go
+++ backend/internal/controller/http/testutils_test.go
@@ -31,6 +31,7 @@ func newTestApp(t *testing.T) (*fiber.App, appDependencies) {
 	mockCommentService := mocks.NewMockCommentService(t)
 	mockRoleService := mocks.NewMockRoleService(t)
 	mockUserService := mocks.NewMockUserService(t)
+	mockSeekerService := mocks.NewMockSeekersService(t)
 	formValidatorService := validator.New(ctx, baseValidator.New())
 
 	mockAuthService.On("GetJWTSecret").Return(secret)
@@ -44,6 +45,7 @@ func newTestApp(t *testing.T) (*fiber.App, appDependencies) {
 		mockUserService,
 		mockRoleService,
 		formValidatorService,
+		mockSeekerService,
 	)
 
 	return app, appDependencies{
