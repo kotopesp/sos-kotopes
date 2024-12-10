80e918fdef628fef9a64863aa2fa43b2277c45ab
Urtix <es.perevalov@mail.ru>
2024-12-11 02:36:16 +0300

    feat(data): Add equipments table
    Signed-off-by: Urtix <es.perevalov@mail.ru>

--- backend/internal/data/000014_seekers.up.sql
+++ backend/internal/data/000014_seekers.up.sql
@@ -1,7 +1,6 @@
 ALTER TABLE IF EXISTS seekers
-    ADD location      VARCHAR   NOT NULL,
-    ADD equipment     VARCHAR   NOT NULL,
-    ADD price         INTEGER   NOT NULL,
-    ADD have_car      BOOLEAN   NOT NULL,
-    ADD is_deleted    BOOLEAN   NOT NULL DEFAULT false,
+    ADD location      VARCHAR     NOT NULL,
+    ADD price         INTEGER     NOT NULL,
+    ADD have_car      BOOLEAN     NOT NULL,
+    ADD is_deleted    BOOLEAN     NOT NULL DEFAULT false,
     ADD deleted_at    TIMESTAMP;
