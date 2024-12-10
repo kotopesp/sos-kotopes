fc2e38f521acbd70fbb020b213545a9b8e713ea0
Urtix <es.perevalov@mail.ru>
2024-12-10 20:08:41 +0300

    feat(router): Add authorization check for creating and updating seeker
    Signed-off-by: Urtix <es.perevalov@mail.ru>

--- backend/internal/controller/http/router.go
+++ backend/internal/controller/http/router.go
@@ -68,8 +68,8 @@ func (r *Router) initRoutes() {
 
 	// seekers
 	v1.Get("/seekers/:id", r.getSeeker)
-	v1.Post("/seekers", r.createSeeker)
-	v1.Post("/seekers", r.updateSeeker)
+	v1.Post("/seekers", r.protectedMiddleware(), r.createSeeker)
+	v1.Post("/seekers", r.protectedMiddleware(), r.updateSeeker)
 
 	// users
 	v1.Get("/users/:id", r.getUser)
