61dd7e9b7ab20e798f1865c6887e5b593e8fb216
Urtix <es.perevalov@mail.ru>
2024-12-11 02:36:16 +0300

    feat(data): Add equipments table
    Signed-off-by: Urtix <es.perevalov@mail.ru>

---
+++ backend/internal/data/000015_equipments.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS
+    equipments
+(
+    id                  SERIAL PRIMARY KEY,
+    have_metal_cage     BOOLEAN,
+    have_plastic_cage   BOOLEAN,
+    have_net            BOOLEAN,
+    have_ladder         BOOLEAN,
+    have_other          VARCHAR
+    );
+
+ALTER TABLE IF EXISTS seekers
+    ADD id_equipment  INTEGER REFERENCES equipments (id);
