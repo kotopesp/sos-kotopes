cba3314b2819c94d2e308854b96cfcd950cd095f
Urtix <es.perevalov@mail.ru>
2024-12-11 20:06:43 +0300

    feat(core): Add equipment type for seeker
    Signed-off-by: Urtix <es.perevalov@mail.ru>

--- backend/internal/core/seeker.go
+++ backend/internal/core/seeker.go
@@ -11,7 +11,7 @@ type (
 		UserID      int       `gorm:"column:user_id"`
 		Description string    `gorm:"column:description"`
 		Location    string    `gorm:"column:location"`
-		Equipment   string    `gorm:"column:equipment"`
+		IdEquipment int       `gorm:"column:id_equipment"`
 		Price       int       `gorm:"column:price"`
 		HaveCar     bool      `gorm:"column:have_car"`
 		CreatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP();column:created_at"`
@@ -25,22 +25,33 @@ type (
 		UserID      int       `gorm:"column:user_id"`
 		Description string    `gorm:"column:description"`
 		Location    string    `gorm:"column:location"`
-		Equipment   string    `gorm:"column:equipment"`
+		IdEquipment int       `gorm:"column:id_equipment"`
 		Price       int       `gorm:"column:price"`
 		HaveCar     bool      `gorm:"column:have_car"`
 		UpdatedAt   time.Time `gorm:"autoUpdateTime;column:updated_at"`
+	}
+
+	Equipment struct {
+		ID              int    `gorm:"primaryKey;autoIncrement;column:id"`
+		HaveMetalCage   bool   `gorm:"column:have_metal_cage"`
+		HavePlasticCage bool   `gorm:"column:have_plastic_cage"`
+		HaveNet         bool   `gorm:"column:have_net"`
+		HaveLadder      bool   `gorm:"column:have_ladder"`
+		HaveOther       string `gorm:"column:have_other"`
 	}
 
 	SeekersService interface {
 		CreateSeeker(ctx context.Context, seeker Seekers) (Seekers, error)
 		GetSeeker(ctx context.Context, id int) (Seekers, error)
 		UpdateSeeker(ctx context.Context, seeker UpdateSeekers) (Seekers, error)
+		CreateEquipment(ctx context.Context, equipment Equipment) (int, error)
 	}
 
 	SeekersStore interface {
 		CreateSeeker(ctx context.Context, seeker Seekers) (Seekers, error)
 		GetSeeker(ctx context.Context, id int) (Seekers, error)
 		UpdateSeeker(ctx context.Context, seeker UpdateSeekers) (Seekers, error)
+		CreateEquipment(ctx context.Context, equipment Equipment) (int, error)
 	}
 )
 
