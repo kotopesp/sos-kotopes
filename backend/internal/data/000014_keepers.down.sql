bc0969f0c21320488cad9c3cbe32f44536c85366
Chirkov Dmitri <chdmitry2@gmail.com>
2024-12-18 12:45:36 +0300

    add: delete columns

--- backend/internal/data/000014_keepers.down.sql
+++ backend/internal/data/000014_keepers.down.sql
@@ -3,4 +3,6 @@ ALTER TABLE IF EXISTS keepers
     DROP COLUMN IF EXISTS boarding_duration,
     DROP COLUMN IF EXISTS boarding_compensation,
     DROP COLUMN IF EXISTS animal_acceptance,
-    DROP COLUMN IF EXISTS animal_category;
+    DROP COLUMN IF EXISTS animal_category,
+    DROP COLUMN IF EXISTS deleted_at,
+    DROP COLUMN IF EXISTS is_deleted;
--- backend/internal/data/000014_keepers.up.sql
+++ backend/internal/data/000014_keepers.up.sql
@@ -3,4 +3,6 @@ ALTER TABLE IF EXISTS keepers
     ADD boarding_duration     VARCHAR NOT NULL,
     ADD boarding_compensation VARCHAR NOT NULL,
     ADD animal_acceptance     VARCHAR NOT NULL,
-    ADD animal_category       VARCHAR NOT NULL;
+    ADD animal_category       VARCHAR NOT NULL,
+    ADD is_deleted            BOOLEAN NOT NULL DEFAULT false,
+    ADD deleted_at            TIMESTAMP;
