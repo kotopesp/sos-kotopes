6a5213c4b5f863ffc1d5ed11890496bd13a053d6
Chirkov Dmitri <chdmitry2@gmail.com>
2024-12-18 13:13:12 +0300

    fix: use enums

--- backend/internal/data/000014_keepers.down.sql
+++ backend/internal/data/000014_keepers.down.sql
@@ -6,3 +6,8 @@ ALTER TABLE IF EXISTS keepers
     DROP COLUMN IF EXISTS animal_category,
     DROP COLUMN IF EXISTS deleted_at,
     DROP COLUMN IF EXISTS is_deleted;
+
+DROP TYPE IF EXISTS animal_acceptance_type CASCADE;
+DROP TYPE IF EXISTS animal_category_type CASCADE;
+DROP TYPE IF EXISTS boarding_compensation_type CASCADE;
+DROP TYPE IF EXISTS boarding_duration_type CASCADE;
