f8a15b332be2fbfafbbceebe4aa9ff976d21a9ee
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-12-22 12:58:14 +0300

    audio-messages: add audio support to message component + audio record button

--- frontend/src/pages/chats-page/ui/message/message.component.ts
+++ frontend/src/pages/chats-page/ui/message/message.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
-import { NgIf } from '@angular/common';
+import {NgIf} from '@angular/common';
+
 @Component({
   selector: 'app-message',
   standalone: true,
@@ -16,4 +17,40 @@ export class MessageComponent {
   messageTime!: string;
   @Input()
   name!: string;
+  @Input()
+  isAudio!: boolean;
+  @Input()
+  audioBytes!: string | null;
+
+  audioBlobUrl = '';
+
+  getAudioUrl() {
+    const blob = this.getBlob();
+    if (blob !== null) {
+      if (this.audioBlobUrl === '') {
+        console.log("Creating new url for audio blob");
+        this.audioBlobUrl = URL.createObjectURL(blob);
+      }
+      return this.audioBlobUrl;
+    } else {
+      return null;
+    }
+  }
+
+  getBlob(): Blob | null {
+    if (this.audioBytes === null) {
+      return null;
+    }
+    const arrayBuffer = this.base64ToArrayBuffer(this.audioBytes);
+    return new Blob([arrayBuffer])
+  }
+
+  private base64ToArrayBuffer(base64: string): ArrayBuffer {
+    const binaryString = atob(base64);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+  }
 }
