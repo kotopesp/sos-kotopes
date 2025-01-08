9a0e62b186f6533ee77dba9871cf4690a6c8a70b
Daniil-Vl <vlasov.daniil03@gmail.com>
2025-01-08 12:49:15 +0300

    audio-messages: implement audio message component with audio waves

--- frontend/src/pages/chats-page/ui/message/message.component.ts
+++ frontend/src/pages/chats-page/ui/message/message.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {NgIf} from '@angular/common';
+import WaveSurfer from "wavesurfer.js";
 
 @Component({
   selector: 'app-message',
@@ -8,7 +9,7 @@ import {NgIf} from '@angular/common';
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
 })
-export class MessageComponent {
+export class MessageComponent implements AfterViewInit {
   @Input()
   answer!: boolean;
   @Input()
@@ -24,6 +25,47 @@ export class MessageComponent {
 
   audioBlobUrl = '';
 
+  @ViewChild("wave") container!: ElementRef;
+  waveSurfer!: WaveSurfer;
+  waveSurferReady = false;
+  audioPlaying = false;
+  currentPlaybackRate = 1;
+
+  ngAfterViewInit(): void {
+    if (this.isAudio) {
+      const options = {
+        "container": this.container.nativeElement,
+        url: this.getAudioUrl()!,
+        "cursorColor": "#333",
+        "progressColor": "#ffffff",
+        "waveColor": "#8c8c8c",
+        "barGap": 1,
+        "barHeight": 1,
+        "barMinHeight": 1,
+        "barRadius": 4,
+        "barWidth": 5,
+        "height": 50,
+        "fillParent": true,
+      }
+
+      this.waveSurfer = WaveSurfer.create(options);
+
+      this.waveSurfer.on('ready', () => {
+        this.waveSurferReady = true;
+      });
+
+      this.waveSurfer.on('pause', () => {
+        this.audioPlaying = false;
+      });
+    }
+  }
+
+  playAudio() {
+    console.log("Play audio");
+    this.waveSurfer.playPause();
+    this.audioPlaying = this.waveSurfer.isPlaying();
+  }
+
   getAudioUrl() {
     const blob = this.getBlob();
     if (blob !== null) {
@@ -34,6 +76,26 @@ export class MessageComponent {
       return this.audioBlobUrl;
     } else {
       return null;
+    }
+  }
+
+  speedUp() {
+    if (this.audioPlaying) {
+      const playbackRate = this.waveSurfer.getPlaybackRate();
+      if (playbackRate < 2) {
+        this.currentPlaybackRate = playbackRate + 0.25;
+        this.waveSurfer.setPlaybackRate(this.currentPlaybackRate);
+      }
+    }
+  }
+
+  speedDown() {
+    if (this.audioPlaying) {
+      const playbackRate = this.waveSurfer.getPlaybackRate();
+      if (playbackRate > 0.25) {
+        this.currentPlaybackRate = playbackRate - 0.25;
+        this.waveSurfer.setPlaybackRate(this.currentPlaybackRate);
+      }
     }
   }
 
@@ -52,5 +114,12 @@ export class MessageComponent {
       bytes[i] = binaryString.charCodeAt(i);
     }
     return bytes.buffer;
+  }
+
+  resetPlaybackRate() {
+    if (this.audioPlaying) {
+      this.currentPlaybackRate = 1;
+      this.waveSurfer.setPlaybackRate(1);
+    }
   }
 }
