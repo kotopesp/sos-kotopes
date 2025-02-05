8c7633e0ac998a9e999e24dfee3ef9c79642ae9c
Dabzelos <dabzelos@gmail.com>
2025-02-06 01:29:17 +0300

    feat: moderator struct and interfaces for storage and service

---
+++ backend/internal/core/moderator.go
@@ -0,0 +1,51 @@
+package core
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+type (
+	Moderator struct {
+		ID           int        `gorm:"column:id"`
+		Username     string     `gorm:"column:username"`
+		Firstname    *string    `gorm:"column:firstname"`
+		Lastname     *string    `gorm:"column:lastname"`
+		PasswordHash string     `gorm:"column:password_hash"`
+		Description  *string    `gorm:"column:description"`
+		IsDeleted    bool       `gorm:"column:is_deleted"`
+		CreatedAt    time.Time  `gorm:"column:created_at"`
+		UpdatedAt    time.Time  `gorm:"column:updated_at"`
+		DeletedAt    *time.Time `gorm:"column:deleted_at"`
+		LastLoginAt  *time.Time `gorm:"column:last_login_at"`
+	}
+	UpdateModerator struct {
+		Name         *string `gorm:"column:username"`
+		Firstname    *string `gorm:"column:firstname"`
+		Lastname     *string `gorm:"column:lastname"`
+		Description  *string `gorm:"column:description"`
+		PasswordHash *string `gorm:"column:password"`
+		Role         *string `gorm:"column:role"`
+	}
+	ModeratorStore interface {
+		UpdateModerator(ctx context.Context, id int, update UpdateModerator) (updatedModerator Moderator, err error)
+		GetModerator(ctx context.Context, id int) (moderator Moderator, err error)
+		GetModeratorByUsername(ctx context.Context, username string) (moderator Moderator, err error)
+		AddModerator(ctx context.Context, moderator Moderator) (moderatorID int, err error)
+	}
+	ModeratorService interface {
+		UpdateModerator(ctx context.Context, id int, update UpdateModerator) (updatedModerator Moderator, err error)
+		GetModerator(ctx context.Context, id int) (moderator Moderator, err error)
+	}
+)
+
+// Errors
+var (
+	ErrNoSuchModerator = errors.New("moderator does not exist")
+)
+
+// TableName table name in db for gorm
+func (Moderator) TableName() string {
+	return "moderators"
+}
