93369dff352e517b0a513ee6825e47563e82526a
Dabzelos <dabzelos@gmail.com>
2025-02-08 00:38:34 +0300

    feat: add moderator method

--- backend/internal/core/moderator.go
+++ backend/internal/core/moderator.go
@@ -19,7 +19,6 @@ type (
 		CreatedAt    time.Time  `gorm:"column:created_at"`
 		UpdatedAt    time.Time  `gorm:"column:updated_at"`
 		DeletedAt    *time.Time `gorm:"column:deleted_at"`
-		LastLoginAt  *time.Time `gorm:"column:last_login_at"`
 	}
 	UpdateModerator struct {
 		Username     *string `gorm:"column:username"`
