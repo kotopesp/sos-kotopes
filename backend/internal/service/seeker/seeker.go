ec49f6d51663ca778b1c1bf685c1cf0e1d343b2e
Efim <es.perevalov@mail.ru>
2025-10-29 00:30:29 +0300

    refactor(store, service): Rework the processing for seekers
    Signed-off-by: Efim <es.perevalov@mail.ru>

--- backend/internal/service/seeker/seeker.go
+++ backend/internal/service/seeker/seeker.go
@@ -3,6 +3,7 @@ package seeker
 import (
 	"context"
 	"errors"
+
 	"github.com/kotopesp/sos-kotopes/internal/core"
 	"github.com/kotopesp/sos-kotopes/pkg/logger"
 )
@@ -15,8 +16,8 @@ func New(seekersStore core.SeekersStore) core.SeekersService {
 	return &service{seekersStore: seekersStore}
 }
 
-func (s *service) CreateSeeker(ctx context.Context, seeker core.Seeker) (core.Seeker, error) {
-	if _, err := s.GetSeeker(ctx, seeker.UserID); !errors.Is(err, core.ErrSeekerNotFound) {
+func (s *service) CreateSeeker(ctx context.Context, seeker core.Seeker) (createSeeker core.Seeker, err error) {
+	if _, err = s.seekersStore.GetSeeker(ctx, seeker.UserID); !errors.Is(err, core.ErrSeekerNotFound) {
 		logger.Log().Error(ctx, core.ErrSeekerExists.Error())
 		return core.Seeker{}, core.ErrSeekerExists
 	}
@@ -26,109 +27,127 @@ func (s *service) CreateSeeker(ctx context.Context, seeker core.Seeker) (core.Se
 
 func (s *service) GetSeeker(ctx context.Context, id int) (core.Seeker, error) {
 	seeker, err := s.seekersStore.GetSeeker(ctx, id)
-	if err != nil {
+	if err != nil || seeker.IsDeleted {
 		logger.Log().Error(ctx, core.ErrSeekerNotFound.Error())
 		return core.Seeker{}, core.ErrSeekerNotFound
-	}
-
-	if seeker.IsDeleted {
-		logger.Log().Error(ctx, core.ErrSeekerDeleted.Error())
-		return core.Seeker{}, core.ErrSeekerDeleted
 	}
 
 	return seeker, nil
 }
 
-func (s *service) UpdateSeeker(ctx context.Context, updateSeeker core.UpdateSeeker) (core.Seeker, error) {
+func createMapUpdates(updateSeeker core.UpdateSeeker, seeker core.Seeker) map[string]interface{} {
 	updates := make(map[string]interface{})
 
-	if updateSeeker.AnimalType != nil {
+	if updateSeeker.AnimalType != nil && seeker.AnimalType != *updateSeeker.AnimalType {
 		updates["animal_type"] = *updateSeeker.AnimalType
 	}
 
-	if updateSeeker.Description != nil {
+	if updateSeeker.Description != nil && seeker.Description != *updateSeeker.Description {
 		updates["description"] = *updateSeeker.Description
 	}
 
-	if updateSeeker.Location != nil {
-		updates["location"] = *updateSeeker.Location
+	if updateSeeker.LocationId != nil && seeker.LocationId != *updateSeeker.LocationId {
+		updates["location_id"] = *updateSeeker.LocationId
 	}
 
-	if updateSeeker.EquipmentRental != nil {
+	if updateSeeker.EquipmentRental != nil && seeker.EquipmentRental != *updateSeeker.EquipmentRental {
 		updates["equipment_rental"] = *updateSeeker.EquipmentRental
 	}
 
-	if updateSeeker.HaveMetalCage != nil {
+	if updateSeeker.HaveMetalCage != nil && seeker.HaveMetalCage != *updateSeeker.HaveMetalCage {
 		updates["have_metal_cage"] = *updateSeeker.HaveMetalCage
 	}
 
-	if updateSeeker.HavePlasticCage != nil {
+	if updateSeeker.HavePlasticCage != nil && seeker.HavePlasticCage != *updateSeeker.HavePlasticCage {
 		updates["have_plastic_cage"] = *updateSeeker.HavePlasticCage
 	}
 
-	if updateSeeker.HaveNet != nil {
+	if updateSeeker.HaveNet != nil && seeker.HaveNet != *updateSeeker.HaveNet {
 		updates["have_net"] = *updateSeeker.HaveNet
 	}
 
-	if updateSeeker.HaveLadder != nil {
+	if updateSeeker.HaveLadder != nil && seeker.HaveLadder != *updateSeeker.HaveLadder {
 		updates["have_ladder"] = *updateSeeker.HaveLadder
 	}
 
-	if updateSeeker.HaveOther != nil {
+	if updateSeeker.HaveOther != nil && seeker.HaveOther != *updateSeeker.HaveOther {
 		updates["have_other"] = *updateSeeker.HaveOther
 	}
 
-	if updateSeeker.Price != nil {
+	if updateSeeker.Price != nil && seeker.Price != *updateSeeker.Price {
 		updates["price"] = *updateSeeker.Price
 	}
 
-	if updateSeeker.HaveCar != nil {
+	if updateSeeker.HaveCar != nil && seeker.HaveCar != *updateSeeker.HaveCar {
 		updates["have_car"] = *updateSeeker.HaveCar
 	}
 
-	if updateSeeker.WillingnessCarry != nil {
+	if updateSeeker.WillingnessCarry != nil && seeker.WillingnessCarry != *updateSeeker.WillingnessCarry {
 		updates["willingness_carry"] = *updateSeeker.WillingnessCarry
 	}
 
-	if len(updates) == 0 {
+	return updates
+}
+
+func (s *service) UpdateSeeker(ctx context.Context, updateSeeker core.UpdateSeeker) (core.Seeker, error) {
+	getSeeker, err := s.GetSeeker(ctx, *updateSeeker.UserID)
+	if err != nil {
+		logger.Log().Error(ctx, core.ErrSeekerNotFound.Error())
+		return core.Seeker{}, core.ErrSeekerNotFound
+	}
+
+	mapUpdates := createMapUpdates(updateSeeker, getSeeker)
+	if len(mapUpdates) == 0 {
 		logger.Log().Error(ctx, core.ErrEmptyUpdateRequest.Error())
 		return core.Seeker{}, core.ErrEmptyUpdateRequest
 	}
 
-	getSeeker, err := s.GetSeeker(ctx, *updateSeeker.UserID)
+	seeker, err := s.seekersStore.UpdateSeeker(ctx, getSeeker.UserID, mapUpdates)
 	if err != nil {
 		logger.Log().Error(ctx, err.Error())
 		return core.Seeker{}, err
 	}
 
-	return s.seekersStore.UpdateSeeker(ctx, getSeeker.ID, updates)
+	return seeker, nil
 }
 
 func (s *service) DeleteSeeker(ctx context.Context, userID int) error {
-	if _, err := s.GetSeeker(ctx, userID); err != nil {
-		logger.Log().Debug(ctx, err.Error())
+	seeker, err := s.GetSeeker(ctx, userID)
+	if err != nil || seeker.IsDeleted {
+		logger.Log().Error(ctx, core.ErrSeekerNotFound.Error())
+		return core.ErrSeekerNotFound
+	}
+
+	if err = s.seekersStore.DeleteSeeker(ctx, userID); err != nil {
+		logger.Log().Error(ctx, err.Error())
 		return err
 	}
 
-	return s.seekersStore.DeleteSeeker(ctx, userID)
+	return nil
+}
+
+func validateParams(params core.GetAllSeekersParams) core.GetAllSeekersParams {
+	sortBy, sortOrder := "created_at", "desc"
+	if params.SortBy == nil {
+		params.SortBy = &sortBy
+	}
+	if params.SortOrder == nil {
+		params.SortOrder = &sortOrder
+	}
+
+	limit, offset := 10, 0
+	if params.Limit == nil {
+		params.Limit = &limit
+	}
+	if params.Offset == nil {
+		params.Offset = &offset
+	}
+
+	return params
 }
 
 func (s *service) GetAllSeekers(ctx context.Context, params core.GetAllSeekersParams) ([]core.Seeker, error) {
-	if params.SortBy == nil {
-		params.SortBy = new(string)
-	}
-	if params.SortOrder == nil {
-		params.SortOrder = new(string)
-	}
-
-	if *params.SortBy == "" {
-		*params.SortBy = "created_at"
-	}
-
-	if *params.SortOrder == "" {
-		*params.SortOrder = "desc"
-	}
-
+	params = validateParams(params)
 	seekers, err := s.seekersStore.GetAllSeekers(ctx, params)
 	if err != nil {
 		logger.Log().Debug(ctx, err.Error())
--- backend/internal/store/seeker/seeker.go
+++ backend/internal/store/seeker/seeker.go
@@ -3,11 +3,13 @@ package seeker
 import (
 	"context"
 	"errors"
+	"time"
+
 	"github.com/kotopesp/sos-kotopes/internal/core"
 	"github.com/kotopesp/sos-kotopes/pkg/logger"
 	"github.com/kotopesp/sos-kotopes/pkg/postgres"
 	"gorm.io/gorm"
-	"time"
+	"gorm.io/gorm/clause"
 )
 
 type store struct {
@@ -18,43 +20,53 @@ func New(pg *postgres.Postgres) core.SeekersStore {
 	return &store{pg}
 }
 
-func (s *store) CreateSeeker(ctx context.Context, seeker core.Seeker) (core.Seeker, error) {
-	tx := s.DB.Begin()
+func (s *store) SeekersDB(ctx context.Context) *gorm.DB {
+	return s.DB.WithContext(ctx).Table("seekers")
+}
+
+func (s *store) CreateSeeker(ctx context.Context, seeker core.Seeker) (createSeeker core.Seeker, err error) {
+	tx := s.SeekersDB(ctx).Begin()
+
 	defer func() {
-		if r := recover(); r != nil {
+		if r := recover(); r != nil || err != nil {
 			tx.Rollback()
 		}
 	}()
 
-	if err := tx.Error; err != nil {
-		logger.Log().Error(ctx, err.Error())
-		return core.Seeker{}, err
-	}
-
 	seeker.CreatedAt = time.Now()
 	seeker.UpdatedAt = time.Now()
 
-	err := tx.WithContext(ctx).Table("seekers").Create(&seeker).Error
-	if err != nil {
-		tx.Rollback()
+	if err = tx.Create(&seeker).Error; err != nil {
 		logger.Log().Error(ctx, err.Error())
 		return core.Seeker{}, err
 	}
 
-	if err = tx.Commit().Error; err != nil {
-		tx.Rollback()
+	if err = tx.
+		Where("user_id = ?", seeker.UserID).
+		Preload("User").
+		First(&seeker).Error; err != nil {
+		if errors.Is(err, core.ErrRecordNotFound) {
+			logger.Log().Debug(ctx, err.Error())
+			return core.Seeker{}, core.ErrNoSuchUser
+		}
+
 		logger.Log().Error(ctx, err.Error())
 		return core.Seeker{}, err
 	}
 
-	return seeker, nil
+	return seeker, tx.Commit().Error
 }
 
 func (s *store) GetSeeker(ctx context.Context, id int) (core.Seeker, error) {
 	var seeker core.Seeker
-	if err := s.DB.WithContext(ctx).Table("seekers").Where("user_id = ?", id).First(&seeker).Error; err != nil {
+
+	err := s.SeekersDB(ctx).
+		Where("user_id = ?", id).
+		Preload("User").
+		First(&seeker).Error
+	if err != nil {
 		if errors.Is(err, core.ErrRecordNotFound) {
-			logger.Log().Error(ctx, core.ErrRecordNotFound.Error())
+			logger.Log().Debug(ctx, err.Error())
 			return core.Seeker{}, core.ErrSeekerNotFound
 		}
 
@@ -65,43 +77,55 @@ func (s *store) GetSeeker(ctx context.Context, id int) (core.Seeker, error) {
 	return seeker, nil
 }
 
-func (s *store) UpdateSeeker(ctx context.Context, id int, updateSeeker map[string]interface{}) (core.Seeker, error) {
-	var seeker core.Seeker
+func (s *store) UpdateSeeker(ctx context.Context, id int, updateSeeker map[string]interface{}) (seeker core.Seeker, err error) {
+	tx := s.SeekersDB(ctx).Begin()
 
-	updateSeeker["updated_at"] = time.Now()
+	defer func() {
+		if r := recover(); r != nil || err != nil {
+			tx.Rollback()
+		}
+	}()
 
-	err := s.DB.WithContext(ctx).Table("seekers").Where("id = ?", id).Updates(updateSeeker).Error
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
+	if err = tx.
+		Where("user_id = ?", id).
+		Preload("User").
+		First(&seeker).Error; err != nil {
+		if errors.Is(err, core.ErrRecordNotFound) {
 			logger.Log().Debug(ctx, err.Error())
 			return core.Seeker{}, core.ErrNoSuchUser
+		}
+
+		logger.Log().Error(ctx, err.Error())
+		return core.Seeker{}, err
+	}
+
+	if err = tx.
+		Model(&seeker).
+		Where("user_id = ?", id).
+		Clauses(clause.Returning{}).
+		Updates(updateSeeker).Error; err != nil {
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			logger.Log().Debug(ctx, err.Error())
+			return core.Seeker{}, core.ErrSeekerNotFound
 		}
 		logger.Log().Error(ctx, err.Error())
 		return core.Seeker{}, err
 	}
 
-	err = s.DB.WithContext(ctx).Table("seekers").Where("id = ?", id).First(&seeker).Error
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			logger.Log().Debug(ctx, err.Error())
-			return core.Seeker{}, core.ErrNoSuchUser
-		}
-		logger.Log().Error(ctx, err.Error())
-		return core.Seeker{}, err
-	}
-
-	return seeker, nil
+	return seeker, tx.Commit().Error
 }
 
 func (s *store) DeleteSeeker(ctx context.Context, userID int) error {
 	updates := make(map[string]interface{})
 	updates["is_deleted"] = true
 
-	err := s.DB.WithContext(ctx).Table("seekers").Where("user_id = ?", userID).Updates(updates).Error
-	if err != nil {
+	if err := s.SeekersDB(ctx).
+		Where("user_id = ?", userID).
+		Updates(updates).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			logger.Log().Debug(ctx, err.Error())
-			return core.ErrNoSuchUser
+			return core.ErrSeekerNotFound
 		}
 		logger.Log().Error(ctx, err.Error())
 		return err
@@ -110,17 +134,15 @@ func (s *store) DeleteSeeker(ctx context.Context, userID int) error {
 	return nil
 }
 
-func (s *store) GetAllSeekers(ctx context.Context, params core.GetAllSeekersParams) ([]core.Seeker, error) {
-	var seekers []core.Seeker
-	query := s.DB.WithContext(ctx).Model(&core.Seeker{})
+func createQuery(query *gorm.DB, params core.GetAllSeekersParams) *gorm.DB {
 	query.Where("is_deleted = false")
 
 	if params.AnimalType != nil {
 		query = query.Where("animal_type = ?", *params.AnimalType)
 	}
 
-	if params.Location != nil {
-		query = query.Where("location = ?", *params.Location)
+	if params.LocationId != nil {
+		query = query.Where("location_id = ?", *params.LocationId)
 	}
 
 	if params.MinPrice != nil {
@@ -164,16 +186,21 @@ func (s *store) GetAllSeekers(ctx context.Context, params core.GetAllSeekersPara
 	}
 
 	query = query.Order(*params.SortBy + " " + *params.SortOrder)
+	query = query.Offset(*params.Offset)
+	query = query.Limit(*params.Limit)
 
-	if params.Offset != nil {
-		query = query.Offset(*params.Offset)
-	}
+	return query
+}
 
-	if params.Limit != nil {
-		query = query.Limit(*params.Limit)
-	}
-
-	if err := query.Preload("User").Find(&seekers).Error; err != nil {
+func (s *store) GetAllSeekers(ctx context.Context, params core.GetAllSeekersParams) ([]core.Seeker, error) {
+	var seekers []core.Seeker
+	query := s.SeekersDB(ctx).Model(&core.Seeker{})
+	query = createQuery(query, params)
+	err := query.Preload("User",
+		func(db *gorm.DB) *gorm.DB {
+			return db.Where("is_deleted = ?", false)
+		}).Find(&seekers).Error
+	if err != nil {
 		logger.Log().Debug(ctx, err.Error())
 		return nil, err
 	}
